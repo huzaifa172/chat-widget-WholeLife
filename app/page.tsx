3a34b67a26d2d3092119a5c3247c15013b1efaff
huzaifa iqbal <huzaifaiqbal5554@gmail.com>
2025-03-19 15:19:41 +0500

    error solved

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,16 @@ export default function Home() {
   return (
    <>
         <Script src="/chat-widget.js"></Script>
-        <Script>{`
+        <Script id="chat-widget-init">{`
         window.onload = function() {
             ChatWidget.init("xx-slkUdka819...");
         };
         `}</Script>
-{/* 
+
       <div>
-      <iframe src="..."></iframe>
+      <iframe src="..." className=""></iframe>
       <button id="trigger-btn"></button>
-    </div> */}
+    </div>
 
    </>
   );
