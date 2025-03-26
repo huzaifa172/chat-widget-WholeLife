face1d824f419fa7962414d289dd0b7bad96e116
huzaifa iqbal <huzaifaiqbal5554@gmail.com>
2025-03-26 15:18:49 +0500

    head changes

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -174,9 +174,9 @@ export default function ChatWidget() {
 
   return (
 <>
-    {/* <Head>
-      {/* <meta name="viewport" content="width=1000; user-scalable=0;" /> */}
-    {/* </Head>  */}
+    <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head> 
     <div className="chat-main-cnt relative bg-transparent max-w-[400px] mt-8 rounded-[10px] border border-[#f59e0b] rounded-[10px] pb-[0px] pt-[0px] mt-[0px] mb-[0px]">
       <div style={{ display: 'flex', alignContent: 'center', alignItems: 'center', marginLeft: '2vh' }} className='bg-transparent'>
           <img
